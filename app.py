598856e167710980118be65b7364959c0649bf5b
Deepthika Sivaram <deepthika.sivaram.edu@gmail.com>
2025-08-01 22:54:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -143,5 +143,6 @@ def upload_image():
     return render_template('upload_image.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
